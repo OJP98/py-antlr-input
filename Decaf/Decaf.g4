34140973249a41ef95e99de69cd58276f7885e0f
OJP98 <ojuarez_p@hotmail.com>
2021-07-14 23:35:38 -0600

    Main program with Decaf example

---
+++ Decaf/Decaf.g4
@@ -0,0 +1,75 @@
+grammar Decaf;
+
+DIGIT: [0-9] ;
+
+LETTER : ('a'..'z'|'A'..'Z'|'_') ;
+
+CHAR : LETTER;
+
+id : LETTER ( LETTER | DIGIT )* ;
+
+num : DIGIT ( DIGIT )* ;
+
+program : 'class' 'Program' '{' declaration* '}' ;
+
+declaration : structDeclaration | varDeclaration | methodDeclaration ;
+
+varDeclaration : varType id ';' | varType id '[' num ']' ';' ;
+
+structDeclaration : 'struct' id '{' varDeclaration* '}' ;
+
+varType : 'int' | 'char' | 'boolean' | 'struct' id | structDeclaration | 'void' ;
+
+methodDeclaration : methodType id '(' (parameter (',' parameter)*)* ')' block ;
+
+methodType : 'int' | 'char' | 'boolean' | 'void' ;
+
+parameter : parameterType id | parameterType id '[' ']' ;
+
+parameterType : 'int' | 'char' | 'boolean' ;
+
+block : '{' (varDeclaration)* (statement)* '}' ;
+
+statement : 'if' '(' expression ')' block ('else' block)?
+          | 'while' '(' expression ')' block
+          | 'return' expression? ';'
+          | methodCall ';'
+          | block
+          | location '=' expression
+          | expression? ';'
+          ;
+
+location : (id | id '[' expression ']') ('.' location)? ;
+
+expression : location
+            | methodCall
+            | literal
+            | expression op expression
+            | '-' expression
+            | '!' expression
+            | '(' expression ')'
+            ;
+
+methodCall : id '(' arg (',' arg)* ')' ;
+
+arg : expression ;
+
+op : arith_op | rel_op | eq_op | cond_op ;
+
+arith_op : '+' | '-' | '*' | '/' | '%' ;
+
+rel_op : '<' | '>' | '=' | '=' ;
+
+eq_op : '==' | '!=' ;
+
+cond_op : '&&' | '||' ;
+
+literal : int_literal | char_literal | bool_literal ;
+
+int_literal : num ;
+
+char_literal : '\'' CHAR '\'' ;
+
+bool_literal : 'true' | 'false' ;
+
+WS : [ \t\n\r] + -> skip ;
---
+++ main.py
@@ -0,0 +1,115 @@
+from tkinter import filedialog as fd, messagebox
+import os
+import tkinter as tk
+
+def SelectFile():
+    return fd.askopenfilename()
+
+def GetGrammarFile():
+    # Get the grammar file for the user
+    grammar_file = SelectFile()
+    while not grammar_file:
+        retry_box = messagebox.askretrycancel(
+            title='Gramática',
+            message='Por favor, seleccione el archivo de gramática'
+        )
+        if not retry_box: exit()
+        grammar_file = SelectFile()
+
+    g_file = grammar_file
+    return os.path.normpath(g_file)
+
+def GetTestFile():
+    # Get the test file from the user
+    test_file = SelectFile()
+    while not test_file:
+        retry_box = messagebox.askretrycancel(
+            title='Archivo de Entrada',
+            message='Por favor, seleccione el archivo de entrada'
+        )
+        if not retry_box: exit()
+        test_file = SelectFile()
+
+    t_file = test_file
+    return os.path.normpath(t_file)
+
+def CompileAndExecute(grammar_path, grammar_name, start_rule, file_path):
+    g_command = f'antlr4 -o . -lib . {grammar_path}'
+    j_command = f'javac *.java'
+    t_command = f'grun {grammar_name} {start_rule} -gui {file_path}'
+
+    os.system(g_command)
+    os.system(j_command)
+    os.system(t_command)
+
+def GetTextInput(label):
+    master = tk.Tk()
+
+    def GetInput():
+        master.withdraw()
+        master.quit()
+
+    tk.Label(master, text=label).pack()
+    # Ask for the user to input the grammar name
+    text_input = tk.Entry(
+        master,
+        justify=tk.LEFT,
+        width=50
+    )
+    text_input.pack()
+    
+    button = tk.Button(
+        master,
+        text='Aceptar',
+        command=GetInput
+    )
+    button.pack()
+
+    master.mainloop()
+
+    return text_input.get()
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.withdraw()
+
+    # Ask for required preparations
+    requirements = messagebox.askyesno(
+        title='Requerimientos',
+        message='¿Está configurado java y ANTLR, junto con los comandos antlr4 y grun?')
+    
+    # If he doesn't have them, then exit
+    if not requirements:
+        messagebox.showerror(
+            title='Error de Requerimientos',
+            message='Por favor, instale los requerimientos para utilizar el programa'
+        )
+        exit()
+
+    grammar_name = GetTextInput('Nombre de la gramática: ')
+
+    # Ask for the user to select the grammar file
+    grammar_box = messagebox.askokcancel(
+        title='Gramática',
+        message='Por favor, seleccione el archivo de gramática'
+    )
+    # exit out if he cancels
+    if not grammar_box: exit()
+
+    grammar_file = GetGrammarFile()
+
+    # start_rule = GetTextInput('Nombre de la regla principal:')
+    start_rule = 'program'
+
+    # Ask for the user to select the input file
+    input_file = messagebox.askokcancel(
+        title='Archivo de entrada',
+        message='Por favor, seleccione el archivo de entrada'
+    )
+    # exit out if he cancels
+    if not input_file: exit()
+
+    test_file = GetTestFile()
+
+    CompileAndExecute(grammar_file, grammar_name, start_rule, test_file)
